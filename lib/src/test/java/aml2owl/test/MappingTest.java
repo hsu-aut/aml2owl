9e6ba520abd5dddaa4bf003e3d1bc678163ac1e7
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2024-06-20 08:49:31 +0200

    Adds additional tests for mapping-object and mirror concept

--- lib/src/test/java/aml2owl/test/MappingTest.java
+++ lib/src/test/java/aml2owl/test/MappingTest.java
@@ -13,12 +13,38 @@ import static org.assertj.core.api.Assertions.*;
 public class MappingTest {
 
 	@Test
-	void testMappingWithoutPlc() throws Exception {
+	void testSucComposition() throws Exception {
 		Path mappingPath = Paths.get("src", "test", "resources", "system-unit-classes", "suc-composition.aml").toAbsolutePath();
 		AmlOwlMapper mapper = new AmlOwlMapper();
 		
 		String actualResult = mapper.executeMappingAndReturnString(mappingPath, null);
 		byte[] fileFromBytes = Files.readAllBytes(Paths.get("src", "test", "resources", "system-unit-classes", "suc-composition.ttl"));
+		String expectedResult = new String(fileFromBytes, StandardCharsets.UTF_8);
+		
+		assertThat(actualResult).isEqualToIgnoringWhitespace(expectedResult);
+	}
+	
+	
+	@Test
+	void testMappingObject() throws Exception {
+		Path mappingPath = Paths.get("src", "test", "resources", "mapping-object", "mapping-object.aml").toAbsolutePath();
+		AmlOwlMapper mapper = new AmlOwlMapper();
+		
+		String actualResult = mapper.executeMappingAndReturnString(mappingPath, null);
+		byte[] fileFromBytes = Files.readAllBytes(Paths.get("src", "test", "resources", "mapping-object", "mapping-object.ttl"));
+		String expectedResult = new String(fileFromBytes, StandardCharsets.UTF_8);
+		
+		assertThat(actualResult).isEqualToIgnoringWhitespace(expectedResult);
+	}
+	
+	
+	@Test
+	void testMirror() throws Exception {
+		Path mappingPath = Paths.get("src", "test", "resources", "mirror", "mirror.aml").toAbsolutePath();
+		AmlOwlMapper mapper = new AmlOwlMapper();
+		
+		String actualResult = mapper.executeMappingAndReturnString(mappingPath, null);
+		byte[] fileFromBytes = Files.readAllBytes(Paths.get("src", "test", "resources", "mirror", "mirror.ttl"));
 		String expectedResult = new String(fileFromBytes, StandardCharsets.UTF_8);
 		
 		assertThat(actualResult).isEqualToIgnoringWhitespace(expectedResult);
