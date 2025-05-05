20e1cbaabe1b31993166b98754b9afd4bf0c81ee
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2025-05-05 15:30:34 +0200

    restructures tests by moving mapping object and mirror into separate test cases

---
+++ lib/src/test/java/aml2owl/test/MirrorTest.java
@@ -0,0 +1,28 @@
+package aml2owl.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.jena.rdf.model.Model;
+import org.junit.jupiter.api.Test;
+
+import aml2owl.core.ResourceLoader;
+import aml2owl.mapping.AmlOwlMapper;
+
+public class MirrorTest {
+
+	@Test
+	void testMirror() throws Exception {
+		Path mappingPath = Paths.get("mirror", "mirror.aml").toAbsolutePath();
+		AmlOwlMapper mapper = new AmlOwlMapper();
+		Model mappedModel= mapper.executeMapping(mappingPath, null);
+		
+		Path expectedModelPath = Paths.get("mirror", "mirror.ttl");
+		Model expectedModel = ResourceLoader.loadResourceAsModel(expectedModelPath);
+		
+		assertTrue(mappedModel.isIsomorphicWith(expectedModel));
+	}
+	
+}
