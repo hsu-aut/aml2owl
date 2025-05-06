4ff9f460c3590a655a827cafa3e94f220c6137ca
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2025-05-06 11:14:40 +0200

    adds comments

--- lib/src/main/java/aml2owl/core/ModelStringWriter.java
+++ lib/src/main/java/aml2owl/core/ModelStringWriter.java
@@ -8,12 +8,16 @@ import org.apache.jena.riot.RDFDataMgr;
 
 public class ModelStringWriter {
 
+	/**
+	 * Converts an RDF model to a Turtle string
+	 * @param model The Apache Jena {@link Model} object to convert
+	 * @return a Turtle-serialized string of the model
+	 */
 	public static String convertModelToString(Model model) {
 		StringWriter writer = new StringWriter();
 		try {
 			RDFDataMgr.write(writer, model, Lang.TURTLE);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		String resultString = writer.toString();
--- lib/src/main/java/aml2owl/core/ResourceLoader.java
+++ lib/src/main/java/aml2owl/core/ResourceLoader.java
@@ -14,6 +14,12 @@ import org.topbraid.jenax.util.JenaUtil;
 
 public class ResourceLoader {
 
+	/**
+	 * Loads an Apache Jena {@link Model} from a file
+	 * @param modelPath {@link Path} to the model to load
+	 * @return A {@link Model} instance 
+	 * @throws IOException In cases the file cannot be found
+	 */
 	public static Model loadResourceAsModel(Path modelPath) throws IOException {
 		Model model = JenaUtil.createMemoryModel();
 		InputStream input = loadResource(modelPath);
@@ -21,25 +27,37 @@ public class ResourceLoader {
 		return model;
 	}
 	
-	public static InputStream loadResource(Path modelPath) throws IOException {
+	/**
+	 * Loads the content of a file as an {@link InputStream}
+	 * @param path {@link Path} to the file to load
+	 * @return An {@link InputStream} instance
+	 * @throws IOException In cases the file cannot be found
+	 */
+	public static InputStream loadResource(Path path) throws IOException {
 	    // First try: Load via classpath (for tests or bundled resources)
-	    String resourceName = "/" + modelPath.toString().replace(File.separatorChar, '/');
+	    String resourceName = "/" + path.toString().replace(File.separatorChar, '/');
 	    InputStream in = ResourceLoader.class.getResourceAsStream(resourceName);
 	    if (in != null) {
 	        return in;
 	    }
 
 	    // Second try. Fallback: file system (for all files, e.g., via CLI)
-	    if (Files.exists(modelPath)) {
-	        return Files.newInputStream(modelPath);
+	    if (Files.exists(path)) {
+	        return Files.newInputStream(path);
 	    }
 
 	    // Last resort: Throw error, if both options don't work
-	    throw new FileNotFoundException("Resource not found on classpath or filesystem: " + modelPath);
+	    throw new FileNotFoundException("Resource not found on classpath or filesystem: " + path);
 	}
 	
-	public static InputStream loadResource(String modelPathString) throws IOException {
-		Path modelPath = Paths.get(modelPathString);
+	/**
+	 * Loads the content of a file as an {@link InputStream}
+	 * @param pathString {@link String} to the file to load
+	 * @return An {@link InputStream} instance
+	 * @throws IOException In cases the file cannot be found
+	 */
+	public static InputStream loadResource(String pathString) throws IOException {
+		Path modelPath = Paths.get(pathString);
 		return loadResource(modelPath);
 	}
 	
